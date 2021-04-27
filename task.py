588d4eb64bf29106f8a989f8148fd735e5063c51
kuangzheng Xia <45323098+JohnyXia001@users.noreply.github.com>
2021-04-27 20:57:14 +1000

    final

---
+++ task.py
@@ -0,0 +1,54 @@
+
+def parse_input(text):
+    temp = text.split(",")
+    date1 = temp[0].lstrip().strip()
+    date2 = temp[1].lstrip().strip()
+
+    date1 = date1.split()
+    date2 = date2.split()
+    date1 = list(map(int, date1))
+    date2 = list(map(int, date2))
+    return date1,date2
+
+def total_days_since1900(date):
+    # parse date
+    d,m,y = date
+    y -= 1900
+
+    # day
+    num_of_days = 0
+    num_of_days += d
+
+    # month
+    monthDays = [31, 28, 31, 30, 31, 30,
+                 31, 31, 30, 31, 30, 31]
+    for i in range(m-1):
+        num_of_days += monthDays[i]
+
+    # year
+    def isleap(year):
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    num_of_days += 365*y
+    for i in range(1900,1901+y,1):
+        if isleap(i):
+            num_of_days += 1
+
+    return num_of_days
+
+def calculate_difference(date1,date2):
+    # input range of dates from 1900 to 2010
+
+    total_day1 = total_days_since1900(date1)
+    total_day2 = total_days_since1900(date2)
+
+    return abs(total_day1-total_day2)
+
+def process(daytext):
+    date1, date2 = parse_input(daytext)
+    diff = calculate_difference(date1, date2)
+    print(diff, 'days')
+
+if __name__ == '__main__':
+    daytext = "01 12 1990, 20 02 2000"
+    daytext = "20 03 2000, 20 02 2000"
+    process(daytext)
